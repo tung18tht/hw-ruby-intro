3197935066a9aabf875b202f7eae1d2961749d04
Tung Nguyen <tung18tht@gmail.com>
2016-04-28 22:25:48 +0700

    Finish part 1

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -1,31 +1,44 @@
-# When done, submit this entire file to the autograder.
-
 # Part 1
 
 def sum arr
-  # YOUR CODE HERE
+    total = 0
+    arr.each{ |number|
+        total += number
+    }
+    total
 end
 
 def max_2_sum arr
-  # YOUR CODE HERE
+    if arr.length == 0
+        return 0
+    elsif arr.length == 1
+        return arr[0]
+    end
+        
+    arr.sort! {|x,y| y <=> x}
+    arr[0] + arr[1]
 end
 
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+    for i in 0...arr.length
+        for j in i+1...arr.length
+            if (arr[i] + arr[j]) == n
+                return true
+            end
+        end
+    end
+    false
 end
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
 end
 
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
 end
 
 # Part 3
